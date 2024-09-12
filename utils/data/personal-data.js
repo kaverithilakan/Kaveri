398fce730b599bbe5303fa49462b59c91a279c7d
Gowrishankarvv <gowrishankarvv1@gmail.com>
2024-09-12 17:10:03 +0530

    hi

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -7,7 +7,7 @@ export const personalData = {
   phone: '+91-8547101180',
   address: 'Alappuzha , Kerala, India - PIN:688530  ',
   github: 'https://github.com/kaverithilakan',
-  facebook: 'https://www.instagram.com/kaverithilakan/',
+  facebook: 'https://medium.com/@kaverithilakan',
   linkedIn: 'https://www.linkedin.com/in/kaverit/',
   twitter: 'https://x.com/KaveriThilakan',
   leetcode: "https://www.instagram.com/kaverithilakan/",
