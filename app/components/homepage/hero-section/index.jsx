98297481308fca9918560c3c6e93f40846929854
Gowrishankarvv <gowrishankarvv1@gmail.com>
2024-09-12 17:16:29 +0530

    FaMedium change hero

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -4,6 +4,7 @@ import { personalData } from "@/utils/data/personal-data";
 import Image from "next/image";
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { FaMedium } from "react-icons/fa";
 import { FaFacebook, FaTwitterSquare,FaInstagram } from "react-icons/fa";
 import { MdDownload } from "react-icons/md";
 import { RiContactsFill } from "react-icons/ri";
@@ -52,7 +53,7 @@ function HeroSection() {
               target='_blank'
               className="transition-all text-pink-500 hover:scale-125 duration-300"
             >
-              <FaFacebook size={30} />
+              <FaMedium size={30} />
             </Link>
             <Link
               href={personalData.leetcode}
